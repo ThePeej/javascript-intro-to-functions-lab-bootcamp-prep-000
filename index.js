a073b776a23c2ac67101a851e3be0a55ebcf7c2f
PJ Frias <peejfrias@gmail.com>
2017-11-02 22:15:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,6 +15,10 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  string.toLowerCase === string
-  return "I can't hear you!"
+  if string.toLowerCase === string {
+    return "I can't hear you!"
+  }
+  elseif string.toUpperCase === string {
+    return "YES INDEED!"
+  }
 }
